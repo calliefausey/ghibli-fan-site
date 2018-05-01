785a1087a17e9772863e790bd44bc162016de24a
hoganavery49 <hogan.avery49@gmail.com>
2018-05-01 10:01:11 -0700

    d

--- webapp.py
+++ webapp.py
@@ -78,67 +78,37 @@ def fanQuiz():
         return render_template('fan-quiz.html', currentScore = score, highScore = session["highScore"], state = 'answer')
     else:
         return render_template('fan-quiz.html', state = 'quiz')
-	
-@app.route('/answerPage',methods=['GET','POST'])
-def renderAnswerPage():
-    #Check answer here
-    session["answer1"] = request.form["answer1"]
-    session["answer2"] = request.form["answer2"]
-    session["answer3"] = request.form["answer3"]
-    session["answer4"] = request.form["answer4"]
-    session["answer5"] = request.form["answer5"]
-    score = 0
-    if session["answer1"].lower() == "hayao miyazaki":
-        score+=1
-    if session["answer2"].lower() == "castle in the sky":
-        score+=1
-    if session["answer3"].lower() == "pig":
-        score+=1
-    if session["answer4"].lower() == "jiji":
-        score+=1
-    if session["answer5"].lower() == "forest":
-        score+=1
-    if "highScore" not in session:
-        session["highScore"] = score
-    elif score > session["highScore"]:
-        session["highScore"] = score
-    return render_template('answerPage.html', currentScore = score, highScore = session["highScore"])
 
 @app.route('/posted', methods=['POST'])
 def post():
-	username = session['user_data']['login']
-	message = request.form['message']
-	try:
-		post = {"username": username, "message": message}
-		post_id = ghibli_forum.insert_one(post).inserted_id
-		post_id
-	except Exception as e:
-		print("error")
-		print(e)
-        
-	return render_template('home.html', past_posts = posts_to_db())
+    username = session['user_data']['login']
+    message = request.form['message']
+    try:
+        collection.insert(
+            {"user": username, "msg": message}
+        )
+    except Exception as e:
+        print('Unable to load database')
+        print(e)
+    return render_template('home.html', posts=posts_to_html())
     
-def posts_to_db():
-	post_table = Markup("<table class='table table-bordered'> <tr> <th> User </th> <th> Ghibli </th> </tr>")
-	try:
-		for p in ghibli_forum.find():
-			post_id= p["_id"]
-			print("Username: " + p["username"] + " Message: " + p["message"])
-			post_table += Markup("<tr> <td>" + p["username"] + "</td> <td>" + p["message"] + "</td> <td>" + "<form action = '/delete' method = 'post'> <button type='submit' name='delete' value='" + str(post_id) + "'>Delete</button></form>" + "</td>")
-	except:
-		print("Error")
-	post_table += Markup("</table>")
-	return post_table
+def posts_to_html():
+    try:
+        table = Markup("<table class='table table-bordered'><tr><th>User</th><th>Post</th><th>Delete</th></tr>")
+        for value in collection.find():
+            table += Markup("<tr><td>" + value["user"] + "</td><td>" + value["msg"] + "</td><td><form action='/delete' method='post'><button type='submit' name='delete' value='" + str(value["_id"]) + "'>Delete</button></form></td></tr>")
+        table += Markup("</table>")
+    except Exception as e:
+        table = Markup('<p>There was an error loading the table data</p>')
+        print(e)
+    return table
     
 @app.route('/delete', methods=['POST'])
 def delete():
-	id=request.form['delete']
-	try:
-		db.ghibli_forum.delete_one( { "_id" : ObjectId(id) } )	
-	except Exception as e:
-		print("error")
-		print(e)
-	return render_template('home.html', past_posts = posts_to_db())
+    #delete posts
+    global collection
+    collection.delete_one({"_id" : ObjectId(request.form['delete'])})
+    return render_template('home.html', posts=posts_to_html())
 	
 @app.route('/login')
 def login():   
